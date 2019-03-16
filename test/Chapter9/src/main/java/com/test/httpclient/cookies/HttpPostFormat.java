c6c4442fcd124c36d20851df2622afd9c8ad262e
yhusr <904239064@qq.com>
2019-03-16 14:45:45 +0800

    接口测试

--- test/Chapter9/src/main/java/com/test/httpclient/cookies/HttpPostFormat.java
+++ test/Chapter9/src/main/java/com/test/httpclient/cookies/HttpPostFormat.java
@@ -14,7 +14,6 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.ResourceBundle;
 
